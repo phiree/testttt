b916409a68b1b4395180025455d2a5199d8f8679
phiree <phiree@gmail.com>
2012-06-21 16:22:00 +0800

    创建用户 的 TDD流程

---
+++ BLL/BLLForTDD/BLLMember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL.BLLForTDD
+{
+
+   public class BLLMember
+    {
+       public bool CreateMember(string username, string password)
+       {
+           return true;
+       }
+    }
+
+}
---
+++ TourTest/TDD/Member/MemberShipTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using BLL.BLLForTDD;
+namespace TourTest.TDD.Member
+{
+    /// <summary>
+    /// 创建用户
+    /// </summary>
+    public  class MemberShipTest
+    {
+        public void CreateTest()
+        {
+            
+            Assert.IsTrue( new BLLMember().CreateMember("yuanfei","password" ));
+        }
+    }
+}
