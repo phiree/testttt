6c5389cc829e9136a6d71514efb9f1aed05b2ac6
sst330381 <sst330381@gmail.com>
2012-07-10 15:09:36 +0800

    add sqlscript

---
+++ CommonLibrary/PDesigner/SEO脚本.sql
@@ -0,0 +1,30 @@
+﻿
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='hangzhou' where [TourOnline].[dbo].[Area].[Id]=1019
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='ningbo' where [TourOnline].[dbo].[Area].[Id]=1034
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='wenzhou' where [TourOnline].[dbo].[Area].[Id]=1047
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='jiaxing' where [TourOnline].[dbo].[Area].[Id]=1060
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='huzhou' where [TourOnline].[dbo].[Area].[Id]=1069
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='shaoxing' where [TourOnline].[dbo].[Area].[Id]=1076
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='jinhua' where [TourOnline].[dbo].[Area].[Id]=1084
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='quzhou' where [TourOnline].[dbo].[Area].[Id]=1095
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='zhoushan' where [TourOnline].[dbo].[Area].[Id]=1103
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='taizhou' where [TourOnline].[dbo].[Area].[Id]=1109
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='lishui' where [TourOnline].[dbo].[Area].[Id]=1120
+  update [TourOnline].[dbo].[Area] set [TourOnline].[dbo].[Area].[SeoName]='shaoxing' where [TourOnline].[dbo].[Area].[Id]=1076
+
+
+
+
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='xiandu' where [TourOnline].[dbo].[Scenic].[Id]=283
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='suichangjinkuang' where [TourOnline].[dbo].[Scenic].[Id]=284
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='nanjianyan' where [TourOnline].[dbo].[Scenic].[Id]=285
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='longquanshan' where [TourOnline].[dbo].[Scenic].[Id]=286
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='shenlongfeipu' where [TourOnline].[dbo].[Scenic].[Id]=287
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='shexiangzhichuang' where [TourOnline].[dbo].[Scenic].[Id]=288
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='qingtianshidiao' where [TourOnline].[dbo].[Scenic].[Id]=289
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='lishuidongxiyan' where [TourOnline].[dbo].[Scenic].[Id]=290
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='lishuihuanglong' where [TourOnline].[dbo].[Scenic].[Id]=291
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='jingningyunzhongdaji' where [TourOnline].[dbo].[Scenic].[Id]=292
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='suichangqianfoshan' where [TourOnline].[dbo].[Scenic].[Id]=293
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='qingtianshimendong' where [TourOnline].[dbo].[Scenic].[Id]=294
+  update [TourOnline].[dbo].[Scenic] set [TourOnline].[dbo].[Scenic].[SeoName]='yunhetitian' where [TourOnline].[dbo].[Scenic].[Id]=295
