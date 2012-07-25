19559e88dc8107011017d802b415a2a3a147e9a8
phiree <phiree@gmail.com>
2012-07-25 10:25:50 +0800

    add reference to mvcproject

--- mvcweb/Controllers/HomeController.cs
+++ mvcweb/Controllers/HomeController.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
 
 namespace mvcweb.Controllers
 {
     public class HomeController : Controller
     {
+
+        /// <summary>
+        /// 首页景区列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List()
+        {
+
+            return View();
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to ASP.NET MVC!";
