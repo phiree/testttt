549e8e57d5c9fc48bf74f9c39c3c9073b08f6777
phiree <phiree@gmail.com>
2012-08-21 09:56:40 +0800

    add Controls project

---
+++ TourControls/ContentReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+namespace TourControls
+{
+    /// <summary>
+    /// 读取html内容,展现在页面上
+    /// </summary>
+    public class ContentReader
+    {
+
+    }
+}
