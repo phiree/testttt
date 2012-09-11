e92d323cfde25001db614262c5a0fefa20d0d382
phiree <phiree@gmail.com>
2012-09-11 11:53:42 +0800

    删除景区后台的文件

--- WEB/ScenicManager/Accounts.aspx
+++
@@ -1,7 +0,0 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/ScenicManager/sm.master" AutoEventWireup="true"
-    CodeFile="Accounts.aspx.cs" Inherits="ScenticManager_Accounts" %>
-
-<asp:Content ID="Content1" ContentPlaceHolderID="smHeader" runat="Server">
-</asp:Content>
-<asp:Content ID="Content2" ContentPlaceHolderID="cphmain" runat="Server">
-</asp:Content>
--- WEB/ScenicManager/UpdateAdminInfo.aspx.cs
+++ WEB/ScenicManager/UpdateAdminInfo.aspx.cs
@@ -6,31 +6,56 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using Model;
 using BLL;
+using System.Web.Security;
 
 public partial class ScenicManager_UploadAdminInfo : basepage
 {
-    BLLMembership bllmembership = new BLLMembership();
+    BLLMembership bllmem = new BLLMembership();
+    TourMembershipProvider tmp = new TourMembershipProvider();
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
         {
             txtusername.Text = CurrentMember.Name;
+            hfName.Value = CurrentMember.Name;
         }
     }
-    protected void Button1_Click(object sender, EventArgs e)
+    protected void BtnInfoOK_Click(object sender, EventArgs e)
     {
-        if (bllmembership.GetMember(txtusername.Text.Trim()) != null && CurrentMember.Name != txtusername.Text.Trim())
+        if (string.IsNullOrWhiteSpace(txtusername.Text.Trim()) || string.IsNullOrWhiteSpace(txtpwd.Text.Trim())
+            || string.IsNullOrWhiteSpace(txtnewpwd1.Text.Trim()))
         {
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('用户名已经存在,请重新填写');", true);
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "string", "alert('信息填写不完整！');", true);
             return;
         }
         else
         {
-            TourMembership tm = CurrentMember;
-            tm.Name = txtusername.Text.Trim();
-            tm.Password = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(txtpwd2.Text.Trim(), "MD5");
-            new BLLMembership().updateinfo(tm);
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('修改成功');", true);
+            TourMembership user = bllmem.GetMember(txtusername.Text.Trim());
+            if (user == null || user.Name==CurrentMember.Name)
+            {
+                TourMembership tm = CurrentMember;
+                tm.Name = txtusername.Text.Trim();
+                //tm.Password = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(txtpwd.Text.Trim(), "MD5");
+                if (tm.Password == System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(txtpwd.Text.Trim(), "MD5"))
+                {
+                    if (txtnewpwd1.Text.Trim() == txtnewpwd2.Text.Trim())
+                    {
+                        tm.Password = System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(txtnewpwd1.Text.Trim(), "MD5");
+                        bllmem.updateinfo(tm);
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('修改成功');window.location='/ScenicManager/'", true);
+                    }
+                }
+                else
+                {
+                    ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('旧密码错误，无法修改！');", true);
+                    return;
+                }
+            }
+            else
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('该用户名已存在，请重新输入！');", true);
+                return;
+            }
         }
-    }
+   }
 }
