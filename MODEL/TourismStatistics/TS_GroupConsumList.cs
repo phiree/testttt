972b1d43ef9a91f19df934a217678dcb3055f257
phiree <phiree@gmail.com>
2012-09-11 11:05:14 +0800

    Add Models for Di Jie She (To be continued)

---
+++ MODEL/TourismStatistics/TS_GroupConsumList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 团队消费列表
+    /// </summary>
+    public class TS_GroupConsumList
+    {
+        public virtual Guid Id { get; set; }
+       // public virtual ts_groupinf
+    }
+}
---
+++ MODEL/TourismStatistics/TS_TourEnterprise.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 旅游企业
+    /// </summary>
+    public class TS_TourEnterprise : TourMembership
+    {
+        /// <summary>
+        /// 旅行社所属区域的区域码
+        /// </summary>
+     
+        public virtual string AreaCode { get; set; }
+        public virtual TourEnterpriseType EnterpriseType { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Phone { get; set; }
+    }
+    public enum TourEnterpriseType
+    {
+        旅行社=1,
+        餐馆,
+        景点,
+        宾馆,
+        饭店
+
+    }
+}
