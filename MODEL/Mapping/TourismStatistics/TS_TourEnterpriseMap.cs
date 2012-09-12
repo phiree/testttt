69f9f02daa23015094d98012bb296a9624589a67
phiree <phiree@gmail.com>
2012-09-12 13:27:44 +0800

    增加地接社相关类的Mapping

---
+++ MODEL/Mapping/TourismStatistics/TS_TourEnterpriseMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Mapping;
+
+namespace Model.Mapping
+{
+    public class TS_TourEnterpriseMap:SubclassMap<TS_TourEnterprise>
+    {
+        public TS_TourEnterpriseMap()
+        { 
+          
+            Map(x => x.Address);
+            Map(x => x.AreaCode);
+            Map(x => x.Phone);
+            Map(x => x.EnterpriseType).CustomType<int>();
+        
+            Map(x => x.Name);
+           
+            
+        }
+    }
+}
