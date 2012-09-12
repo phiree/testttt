da01d366df68e80e018a7e1eed10bacd96596bf1
phiree <phiree@gmail.com>
2012-09-12 18:18:54 +0800

    低级社游客信息

---
+++ MODEL/TourismStatistics/TS_VisitorInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    /// <summary>
+    /// 游客信息
+    /// </summary>
+    public class TS_Manager : TourMembership
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        //身份证号码
+        public virtual string CardNo { get; set; }
+
+    }
+}
