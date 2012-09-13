ca7eccdd7703db7ca248be421b2611b39f905b75
phiree <phiree@gmail.com>
2012-09-13 16:23:26 +0800

    回复到以前版本

--- WEB/Order/CheckOut.aspx.cs
+++ WEB/Order/CheckOut.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -33,59 +33,12 @@ public partial class Scenic_CheckOut : AuthPage
         rptCart.DataSource = tickets;
         rptCart.DataBind();
     }
-    BLLScenicTicket bllST = new BLLScenicTicket();
     private void BindAssign()
     {
-        // #unionticket
-        IList<AssignedScenic> assScenics = new List<AssignedScenic>();
-        foreach (Ticket t in tickets)
-        {
-            IList<Scenic> scenics = bllST.GetScenicByTicket(t.Id);
-            bool isInUnion = scenics.Count >= 2;
-            if (!isInUnion)
-            {
-                scenics.Add(t.Scenic);
-            }
-
-            foreach (Scenic s in scenics)
-            {
-
-                AssignedScenic assScenic = new AssignedScenic();
-
-
-                assScenic.IsInUnion = isInUnion;
-                assScenic.TicketId = t.Id;
-                assScenic.Scenic = s;
-
-                if (!assScenics.Contains(assScenic))
-                {
-                    assScenics.Add(assScenic);
-                }
-            }
-
-        }
-        rptAssign.DataSource = assScenics;
-        // rptAssign.DataSource = tickets;
+        rptAssign.DataSource = tickets;
         rptAssign.DataBind();
     }
 
-    private class AssignedScenic : IComparable
-    {
-        public int TicketId { get; set; }
-        public Scenic Scenic { get; set; }
-        public bool IsInUnion { get; set; }
-
-
-
-        public int CompareTo(object obj)
-        {
-            var objAS = (AssignedScenic)obj;
-            if (objAS.TicketId == TicketId && Scenic.Id == Scenic.Id)
-                return 0;
-            else return 1;
-
-        }
-    }
 
     protected void rptCart_ItemDataBound(object sender, RepeaterItemEventArgs e)
     {
@@ -113,14 +66,4 @@ public partial class Scenic_CheckOut : AuthPage
     /// 绑定 门票分配里的 门票列表
     /// </summary>
 
-    protected void rptAssign_ItemDataBound(object sender, RepeaterItemEventArgs e)
-    {
-        if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
-        {
-            AssignedScenic t = e.Item.DataItem as AssignedScenic;
-
-            Label lbl = e.Item.FindControl("lblUnion") as Label;
-            lbl.Visible = t.IsInUnion;
-        }
-    }
 }
