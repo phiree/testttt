b4e4784e202b2fbd8342782c8832d9f872538d98
phiree <phiree@gmail.com>
2012-09-14 18:14:25 +0800

    景区管理员登录提示/ 套票管理(未完)

--- WEB/Manager/ScenicManage/UnionTicket/UnionTicketEdit.aspx.cs
+++ WEB/Manager/ScenicManage/UnionTicket/UnionTicketEdit.aspx.cs
@@ -7,6 +7,13 @@ using System.Web.UI.WebControls;
 
 public partial class Manager_ScenicManage_UnionTicket_UnionTicketEdit : System.Web.UI.Page
 {
+    /// <summary>
+    /// 创建/编辑套票信息
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    int ticketId;
+
     protected void Page_Load(object sender, EventArgs e)
     {
 
