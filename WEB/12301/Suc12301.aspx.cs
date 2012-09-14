e650370227c283ab964ee7b51636ecc57cefb1ba
sst330381 <sst330381@gmail.com>
2012-09-14 17:06:27 +0800

    快速注册成功信息修改

--- WEB/12301/Suc12301.aspx.cs
+++ WEB/12301/Suc12301.aspx.cs
@@ -7,8 +7,12 @@ using System.Web.UI.WebControls;
 
 public partial class _12301_Suc12301 : System.Web.UI.Page
 {
+    public string phone;
+    public string psw;
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        phone = Request["phone"];
+        psw = Request["psw"];
     }
 }
