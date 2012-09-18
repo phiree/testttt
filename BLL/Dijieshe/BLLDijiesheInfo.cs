426fbaccb12ead169cc6d5d8d0da06c000763c1a
phiree <phiree@gmail.com>
2012-09-18 16:24:39 +0800

    增加地接社网站

---
+++ BLL/Dijieshe/BLLDijiesheInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Model;
+namespace BLL.Dijieshe
+{
+   public class BLLDijiesheInfo
+    {
+       
+    }
+}
