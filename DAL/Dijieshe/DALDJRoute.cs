f6b45c1a3c4943aeea439013ada2b4383b1711e8
phiree <phiree@gmail.com>
2012-09-26 11:48:15 +0800

    实现数据层

---
+++ DAL/Dijieshe/DALDJRoute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using Model;
+namespace DAL
+{
+    public class DALDJ_Route:DalBase,IDAL.IDJRoute
+    {
+
+        public Model.DJ_Route GetById(Guid routeId)
+        {
+            return session.Get<DJ_Route>(routeId);
+        }
+
+        public void SaveOrUpdate(Model.DJ_Route route)
+        {
+            session.SaveOrUpdate(route);
+        }
+
+        public void Delete(Model.DJ_Route route)
+        {
+            session.Delete(route);
+        }
+    }
+}
