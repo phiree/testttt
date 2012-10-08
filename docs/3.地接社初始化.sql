107706e7abaea3929391cc4a74fac5678b8faa01
foreverjjj <502224593@qq.com>
2012-10-08 17:36:38 +0800

    加入了团队初始化脚本

--- docs/3.地接社初始化.sql
+++ docs/3.地接社初始化.sql
@@ -30,3 +30,10 @@ INSERT INTO [TourOnline].[dbo].[DJ_Group_Driver]
 VALUES('B889DEA4-D3A1-4A48-BBF3-A0D20110BD1C','浙A00001')
 INSERT INTO [TourOnline].[dbo].[DJ_Group_Driver]
 VALUES('B889DEA4-D3A1-4A48-BBF3-A0D20110BD1D','浙A00001')
+
+--团队信息
+insert into DJ_TourEnterprise values('杭州地接社','西溪路358号','老王','330203515695','874569253',1,5,1019)
+insert into DJ_dijiesheInfo values(438)
+insert into DJ_TourGroup values('e4fd78ba-9ed6-49a4-9331-66f93d4f85b7','浙江杭州三日游','2012-10-7','2012-10-9',3,20,3,438)
+insert into DJ_Group_Worker values(newid(),'330203198705265484','徐老六','18992548675','54786523q',1,'e4fd78ba-9ed6-49a4-9331-66f93d4f85b7')
+insert into DJ_Route values(newid(),1,8,15,'参观江郎山景区',435,null,'e4fd78ba-9ed6-49a4-9331-66f93d4f85b7')
