55d43437028a32cdddbb05ed9a9b289a3b8b6c9a
sst330381 <sst330381@gmail.com>
2012-10-12 15:23:13 +0800

    权限

--- DiJieSheWeb/Groups/Groups.master.cs
+++ DiJieSheWeb/Groups/Groups.master.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using BLL;
+using Model;
 
 public partial class Groups_Groups : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected override void OnInit(EventArgs e)
+    {
+        MembershipUser mu = Membership.GetUser();
+        BLLDJ_User blldj_user = new BLLDJ_User();
+        DJ_User_TourEnterprise DJ_User_Ent = null;
+        if (mu != null)
+        {
+            DJ_User_Ent = new BLLMembership().GetMemberById((Guid)mu.ProviderUserKey) as DJ_User_TourEnterprise;
+        }
+        if (mu == null || mu.UserName == string.Empty || DJ_User_Ent == null)
+        {
+            Response.Redirect("/Login.aspx");
+        }
+        base.OnInit(e);
     }
 }
