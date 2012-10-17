f0c09fad3b38dbee916b3ed7ca88807be3c01b7a
foreverjjj <502224593@qq.com>
2012-10-17 16:32:23 +0800

    预知bug

--- BLL/Dijieshe/BLLDJRoute.cs
+++ BLL/Dijieshe/BLLDJRoute.cs
@@ -15,10 +15,10 @@ namespace BLL
         /// <param name="time">预报时间</param>
         /// <param name="teid">企业id</param>
         /// <returns>团队列表</returns>
-        public IList<Model.DJ_TourGroup> GetTgByTime(DateTime time, int teid)
+        public IList<Model.DJ_Route> GetTgByTime(DateTime time, int teid)
         {
             List<Model.DJ_TourGroup> ListAllTg = Idjroute.GetNotendGroup().ToList();
-            List<Model.DJ_TourGroup> ListTg = new List<Model.DJ_TourGroup>();
+            List<Model.DJ_Route> ListRoute = new List<Model.DJ_Route>();
             foreach (Model.DJ_TourGroup tg in ListAllTg)
             {
                 foreach (Model.DJ_Route route in tg.Routes)
@@ -26,11 +26,11 @@ namespace BLL
                     if (DateTime.Parse(tg.BeginDate.AddDays(route.DayNo - 1).ToShortDateString()) <= DateTime.Parse(time.ToShortDateString()) && DateTime.Parse(tg.BeginDate.AddDays(route.DayNo - 1).ToShortDateString())>=DateTime.Parse(DateTime.Now.ToShortDateString()) && DateTime.Parse(tg.EndDate.ToShortDateString()) >= DateTime.Parse(DateTime.Now.ToShortDateString()) && route.Enterprise.Id == teid)
                     {
                         if (new DAL.DALDJ_GroupConsumRecord().GetGroupConsumRecordByRouteId(route.Id)==null)
-                            ListTg.Add(tg);
+                            ListRoute.Add(route);
                     }
                 }
             }
-            return ListTg;
+            return ListRoute;
         }
 
         public Model.DJ_Route GetById(Guid routeId)
--- DiJieSheWeb/TourEnterprise/TEGroupForecast.aspx.cs
+++ DiJieSheWeb/TourEnterprise/TEGroupForecast.aspx.cs
@@ -48,13 +48,8 @@ public partial class TourEnterprise_TEGroupForecast : System.Web.UI.Page
         if (e.Item.ItemType == ListItemType.AlternatingItem || e.Item.ItemType == ListItemType.Item)
         {
             Literal laArriveTime = e.Item.FindControl("ArriveTime") as Literal;
-            DJ_TourGroup group = e.Item.DataItem as DJ_TourGroup;
-            List<DJ_Route> ListRoute = group.Routes.Where(x => x.Enterprise.Id == Master.CurrentTE.Id).ToList<DJ_Route>();
-            if (ListRoute[Index] != null)
-            {
-                laArriveTime.Text = group.BeginDate.AddDays(ListRoute[Index].DayNo - 1).ToString("yyyy年MM月dd日");
-                Index++;
-            }
+            DJ_Route route = e.Item.DataItem as DJ_Route;
+            laArriveTime.Text = route.DJ_TourGroup.BeginDate.AddDays(route.DayNo - 1).ToString("yyyy年MM月dd日");
         }
     }
 }
