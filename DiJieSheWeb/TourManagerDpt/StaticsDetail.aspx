30bb0ed5c6f51454329ce7b702e32bfa97cf56ff
sst330381 <sst330381@gmail.com>
2012-10-30 15:25:58 +0800

    修改统计信息

--- DiJieSheWeb/TourManagerDpt/StaticsDetail.aspx
+++ DiJieSheWeb/TourManagerDpt/StaticsDetail.aspx
@@ -2,47 +2,60 @@
     AutoEventWireup="true" CodeFile="StaticsDetail.aspx.cs" Inherits="TourManagerDpt_StaticsDetail" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="main" runat="Server">
-    <asp:Repeater ID="rptStaticDetail" runat="server" OnItemDataBound="rptStaticDetail_ItemDataBound">
-        <HeaderTemplate>
-            <table>
-                <thead>
-                    <tr>
-                        <td>
-                            日期
-                        </td>
-                        <td>
-                            住宿人数
-                        </td>
-                        <td>
-                            游览人数
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
-        </HeaderTemplate>
-        <ItemTemplate>
-            <tr>
-                <td>
-                    <%#Eval("ConsumeDate") %>
-                </td>
-                <td>
-                    <asp:Repeater ID="rptHotels" runat="server">
-                        <ItemTemplate>
-                            <%#Eval("Enterprice.Name")%>：<%#Eval("Peoplenum")%>，
-                        </ItemTemplate>
-                    </asp:Repeater>
-                </td>
-                <td>
-                    <asp:Repeater ID="rptScenics" runat="server">
-                        <ItemTemplate>
-                            <%#Eval("Enterprice.Name")%>：<%#Eval("Peoplenum")%>，
-                        </ItemTemplate>
-                    </asp:Repeater>
-                </td>
-            </tr>
-        </ItemTemplate>
-        <FooterTemplate>
-            </tbody> </table>
-        </FooterTemplate>
-    </asp:Repeater>
+    <div class="detaillist">
+        <div class="detailtitle">
+            已接待情况
+        </div>
+        <asp:Repeater ID="rptStaticDetail" runat="server" OnItemDataBound="rptStaticDetail_ItemDataBound">
+            <HeaderTemplate>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>
+                                日期
+                            </td>
+                            <td>
+                                住宿人数
+                            </td>
+                            <td>
+                                游览人数
+                            </td>
+                        </tr>
+                    </thead>
+                    <tbody>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td>
+                        <%#Eval("ConsumeDate") %>
+                    </td>
+                    <td>
+                        <ul>
+                            <asp:Repeater ID="rptHotels" runat="server">
+                                <ItemTemplate>
+                                    <li>
+                                        <%#Eval("Enterprice.Name")%>：<%#Eval("Peoplenum")%>
+                                    </li>
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </ul>
+                    </td>
+                    <td>
+                        <ul>
+                            <asp:Repeater ID="rptScenics" runat="server">
+                                <ItemTemplate>
+                                    <li>
+                                        <%#Eval("Enterprice.Name")%>：<%#Eval("Peoplenum")%>
+                                    </li>
+                                </ItemTemplate>
+                            </asp:Repeater>
+                        </ul>
+                    </td>
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                </tbody> </table>
+            </FooterTemplate>
+        </asp:Repeater>
+    </div>
 </asp:Content>
--- DiJieSheWeb/TourManagerDpt/StaticsList.aspx.cs
+++ DiJieSheWeb/TourManagerDpt/StaticsList.aspx.cs
@@ -11,6 +11,12 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
     BLL.BLLDJTourGroup blltg = new BLL.BLLDJTourGroup();
     public int xuhao_orig = 1;
     public int xuhao_1 = 1;
+    public int m_total = 0;
+    public int m_hotel = 0;
+    public int m_play = 0;
+    public int y_total = 0;
+    public int y_hotel = 0;
+    public int y_play = 0;
     public int xuhao_2 = 1;
     public int xuhao_3 = 1;
 
@@ -25,13 +31,8 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
     private void BindData()
     {
         txt_yijiedai.Text = DateTime.Now.Year + "-" + DateTime.Now.Month + "-1";
-        txt_yijiedai_end.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddDays(-1).ToShortDateString();
         txt_yijiedai2.Text = DateTime.Now.Year + "-" + DateTime.Now.Month + "-1";
-        txt_yijiedai2_end.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).AddMonths(1).AddDays(-1).ToShortDateString();
-
-        //原数据
-        rptOrigin.DataSource = bllCustomRecord.GetGCR8Multi(null, null, null, null, null);
-        rptOrigin.DataBind();
+        txt_yijiedai3.Text = DateTime.Now.Year + "-" + DateTime.Now.Month + "-1";
 
         //整理后数据Gov1
         BindGov1();
@@ -59,33 +60,46 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
             gcrlist = gcrlist.Where(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 6))).ToList();
         }
         IList<Model.DJ_GroupConsumRecord> gcrlist_month;
+        IList<Model.DJ_GroupConsumRecord> gcrlist_year;
         IList<statics_model> sm1 = new List<statics_model>();
         //V.2012.10.26
-        //string[] temp = txt_yijiedai.Text.Split(new char[] { '-', '/' });
-        //if (!string.IsNullOrEmpty(txt_yijiedai.Text) && temp.Length >= 2)
-        //{
-        //    var begin_month = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), 1);
-        //    var end_month = begin_month.AddMonths(1);
-        //    var begin_year = new DateTime(int.Parse(temp[0]), 1, 1);
-        //    var end_year = begin_year.AddYears(1);
-        //    gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
-        //    gcrlist_year = gcrlist.Where(x => x.ConsumeTime >= begin_year && x.ConsumeTime < end_year).ToList();
-        //}
-        //else
-        //{
-        //    var begin_month = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
-        //    var end_month = begin_month.AddMonths(1);
-        //    var begin_year = new DateTime(DateTime.Today.Year, 1 ,1);
-        //    var end_year = begin_month.AddYears(1);
-        //    gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
-        //    gcrlist_year = gcrlist.Where(x => x.ConsumeTime >= begin_year && x.ConsumeTime < end_year).ToList();
-        //}
-        //V.2012.10.27
         string[] temp = txt_yijiedai.Text.Split(new char[] { '-', '/' });
-        string[] temp2 = txt_yijiedai_end.Text.Split(new char[] { '-', '/' });
-        var begin_month = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
-        var end_month = new DateTime(int.Parse(temp2[0]), int.Parse(temp2[1]), int.Parse(temp2[2])).AddDays(1);
-        gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
+        if (!string.IsNullOrEmpty(txt_yijiedai.Text) && temp.Length >= 2)
+        {
+            var begin_month = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), 1);
+            var end_month = begin_month.AddMonths(1);
+            var begin_year = new DateTime(int.Parse(temp[0]), 1, 1);
+            var end_year = begin_month.AddMonths(1);
+            gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
+            gcrlist_year = gcrlist.Where(x => x.ConsumeTime >= begin_year && x.ConsumeTime < end_year).ToList();
+        }
+        else
+        {
+            var begin_month = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var end_month = begin_month.AddMonths(1);
+            var begin_year = new DateTime(DateTime.Today.Year, 1, 1);
+            var end_year = begin_month.AddMonths(1);
+            gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
+            gcrlist_year = gcrlist.Where(x => x.ConsumeTime >= begin_year && x.ConsumeTime < end_year).ToList();
+        }
+        //V.2012.10.27
+        //string[] temp = txt_yijiedai.Text.Split(new char[] { '-', '/' });
+        //string[] temp2 = txt_yijiedai_end.Text.Split(new char[] { '-', '/' });
+        //var begin_month = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+        //var end_month = new DateTime(int.Parse(temp2[0]), int.Parse(temp2[1]), int.Parse(temp2[2])).AddDays(1);
+        //gcrlist_month = gcrlist.Where(x => x.ConsumeTime >= begin_month && x.ConsumeTime < end_month).ToList();
+        //foreach (var item in gcrlist_month.GroupBy(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Name))
+        //{
+        //    sm1.Add(new statics_model()
+        //    {
+        //        Name = item.Key,
+        //        AdultsAmount = item.Sum(x => x.AdultsAmount),
+        //        ChildrenAmount = item.Sum(x => x.ChildrenAmount),
+        //        LiveDays = item.Sum(x => (x.AdultsAmount + x.ChildrenAmount) * x.LiveDay),
+        //        Playnums = item.Sum(x => x.AdultsAmount + x.ChildrenAmount)
+        //    });
+        //}
+        //V.2012.10.26
         foreach (var item in gcrlist_month.GroupBy(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Name))
         {
             sm1.Add(new statics_model()
@@ -97,26 +111,28 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
                 Playnums = item.Sum(x => x.AdultsAmount + x.ChildrenAmount)
             });
         }
-        //V.2012.10.26
-        //foreach (var item in gcrlist_year.GroupBy(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Name))
-        //{
-        //    var static_m=sm1.Where(x => x.Name == item.Key);
-        //    if(static_m.Count()>0)
-        //    { 
-        //        static_m.First().y_AdultsAmount=item.Sum(x => x.AdultsAmount);
-        //        static_m.First().y_ChildrenAmount = item.Sum(x => x.ChildrenAmount);
-        //        static_m.First().y_LiveDays = item.Sum(x => (x.AdultsAmount + x.ChildrenAmount) * x.LiveDay);
-        //        static_m.First().y_Playnums = item.Sum(x => x.AdultsAmount + x.ChildrenAmount);
-        //    }
-        //}
+        foreach (var item in gcrlist_year.GroupBy(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Name))
+        {
+            var static_m = sm1.Where(x => x.Name == item.Key);
+            if (static_m.Count() > 0)
+            {
+                static_m.First().y_AdultsAmount = item.Sum(x => x.AdultsAmount);
+                static_m.First().y_ChildrenAmount = item.Sum(x => x.ChildrenAmount);
+                static_m.First().y_LiveDays = item.Sum(x => (x.AdultsAmount + x.ChildrenAmount) * x.LiveDay);
+                static_m.First().y_Playnums = item.Sum(x => x.AdultsAmount + x.ChildrenAmount);
+            }
+        }
         if (!string.IsNullOrEmpty(txt_name1.Text))
         {
-            rptGov1.DataSource = sm1.Where(x => x.Name.Split(new string[] { txt_name1.Text }, StringSplitOptions.None).Count() > 1);
+            sm1 = sm1.Where(x => x.Name.Split(new string[] { txt_name1.Text }, StringSplitOptions.None).Count() > 1).ToList();
         }
-        else
-        {
-            rptGov1.DataSource = sm1;
-        }
+        m_total = sm1.Sum(x => x.AdultsAmount) + sm1.Sum(x => x.ChildrenAmount);
+        m_hotel = sm1.Sum(x => x.LiveDays);
+        m_play = sm1.Sum(x => x.Playnums);
+        y_total = sm1.Sum(x => x.y_AdultsAmount) + sm1.Sum(x => x.y_ChildrenAmount);
+        y_hotel = sm1.Sum(x => x.y_LiveDays);
+        y_play = sm1.Sum(x => x.y_Playnums);
+        rptGov1.DataSource = sm1;
         rptGov1.DataBind();
     }
 
@@ -135,27 +151,32 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
         {
             gcrlist = gcrlist.Where(x => x.Route.DJ_TourGroup.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 6))).ToList();
         }
-        IList<statics_model> sm1 = new List<statics_model>();
         //V.2012.10.26
-        //string[] temp = txt_yijiedai2.Text.Split(new char[] { '-', '/' });
-        //if (!string.IsNullOrEmpty(txt_yijiedai2.Text) && temp.Length >= 2)
-        //{
-        //    var begin_date = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), 1);
-        //    var end_date = begin_date.AddMonths(1).AddDays(-1);
-        //    gcrlist = gcrlist.Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date).ToList();
-        //}
-        //else
-        //{
-        //    var begin_date = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
-        //    var end_date = begin_date.AddMonths(1).AddDays(-1);
-        //    gcrlist = gcrlist.Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date).ToList();
-        //}
-        //V.2012.10.27
         string[] temp = txt_yijiedai2.Text.Split(new char[] { '-', '/' });
-        string[] temp2 = txt_yijiedai2_end.Text.Split(new char[] { '-', '/' });
-        var begin_date = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
-        var end_date = new DateTime(int.Parse(temp2[0]), int.Parse(temp2[1]), int.Parse(temp2[2])).AddDays(1);
-        gcrlist = gcrlist.Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date).ToList();
+        if (!string.IsNullOrEmpty(txt_yijiedai2.Text) && temp.Length >= 2)
+        {
+            var begin_date = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), 1);
+            var end_date = begin_date.AddMonths(1);
+            gcrlist = gcrlist
+                .Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date)
+                .Where(x => x.Enterprise.Name == txt_name2.Text)
+                .ToList();
+        }
+        else
+        {
+            var begin_date = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
+            var end_date = begin_date.AddMonths(1).AddDays(-1);
+            gcrlist = gcrlist
+                .Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date)
+                .Where(x => x.Enterprise.Name == txt_name2.Text)
+                .ToList();
+        }
+        //V.2012.10.27
+        //string[] temp = txt_yijiedai2.Text.Split(new char[] { '-', '/' });
+        //string[] temp2 = txt_yijiedai2_end.Text.Split(new char[] { '-', '/' });
+        //var begin_date = new DateTime(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]));
+        //var end_date = new DateTime(int.Parse(temp2[0]), int.Parse(temp2[1]), int.Parse(temp2[2])).AddDays(1);
+        //gcrlist = gcrlist.Where(x => x.ConsumeTime >= begin_date && x.ConsumeTime < end_date).ToList();
 
         //整理后数据Gov2
         IList<statics_Gov2> sm2 = new List<statics_Gov2>();
@@ -171,14 +192,7 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
 
             });
         }
-        if (!string.IsNullOrEmpty(txt_name2.Text))
-        {
-            rptGov2.DataSource = sm2.Where(x => x.Name.Split(new string[] { txt_name2.Text }, StringSplitOptions.None).Count() > 1);
-        }
-        else
-        {
-            rptGov2.DataSource = sm2;
-        }
+        rptGov2.DataSource = sm2;
         rptGov2.DataBind();
     }
 
@@ -188,53 +202,115 @@ public partial class TourManagerDpt_StaticsList : basepageMgrDpt
         IList<Model.DJ_TourGroup> tglist = blltg.GetTourGroupByAll();
         if (CurrentDptLevel == "1")
         {
-            tglist=tglist.Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 2))).ToList();
+            tglist = tglist
+                .Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 2)))
+                .ToList();
         }
         if (CurrentDptLevel == "2")
         {
-            tglist = tglist.Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 4))).ToList();
+            tglist = tglist
+                .Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 4)))
+                .ToList();
         }
         if (CurrentDptLevel == "3")
         {
-            tglist = tglist.Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 6))).ToList();
+            tglist = tglist
+                .Where(x => x.DJ_DijiesheInfo.Area.Code.StartsWith(CurrentDpt.Area.Code.Substring(0, 6)))
+                .ToList();
+        }
+        //筛选企业
+        if (!string.IsNullOrEmpty(txt_name3djs.Text))
+        {
+            tglist = tglist.Where(x => x.Routes.Where(y => y.Enterprise.Name == txt_name3djs.Text.Trim()).Count() > 0).ToList();
+        }
+        //筛选日期
+        if (!string.IsNullOrEmpty(txt_yijiedai3.Text))
+        {
+            tglist = tglist.Where(x => x.BeginDate <= DateTime.Parse(txt_yijiedai3.Text) && x.EndDate > DateTime.Parse(txt_yijiedai3.Text)).ToList();
         }
         IList<statics_Gov3> sm3 = new List<statics_Gov3>();
         foreach (var item3 in tglist.Where(x => x.DJ_DijiesheInfo != null))
         {
-            sm3.Add(new statics_Gov3()
+            var temp = new statics_Gov3();
+            temp.Name = item3.DJ_DijiesheInfo.Name;
+            temp.Gname = item3.Name;
+            temp.GId = item3.Id.ToString();
+            temp.Bedate = item3.BeginDate.ToShortDateString() + "~" + item3.EndDate.ToShortDateString();
+
+            var temp_y_hotel = item3.Routes
+                .Where(x => x.Enterprise.Type == Model.EnterpriseType.宾馆)
+                .Where(x => x.DayNo == ((DateTime.Parse(txt_yijiedai3.Text) - item3.BeginDate).Days));
+            if (temp_y_hotel.Count() > 0)
             {
-                Name = item3.DJ_DijiesheInfo.Name,
-                Gname = item3.Name,
-                GId = item3.Id.ToString(),
-                Bedate = item3.BeginDate.ToShortDateString() + "-" + item3.EndDate.ToShortDateString()
-            });
-        }
-        //团队名字非空
-        if (!string.IsNullOrEmpty(txt_name3.Text))
-        {
-            //地接社名字非空
-            if (!string.IsNullOrEmpty(txt_name3djs.Text))
-            {
-                sm3 = sm3.Where(x => x.Name.Split(new string[] { txt_name3djs.Text }, StringSplitOptions.None).Count() > 1).ToList();
-                rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3.Text }, StringSplitOptions.None).Count() > 1);
+                foreach (var item in temp_y_hotel)
+                {
+                    temp.y_hotel += item.Enterprise.Name + " ";
+                }
             }
             else
             {
-                rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3.Text }, StringSplitOptions.None).Count() > 1);
+                temp.t_hotel = "无";
             }
-        }
-        //团队名字空
-        else
-        {
-            if (!string.IsNullOrEmpty(txt_name3djs.Text))
+
+            var temp_t_hotel=item3.Routes
+                .Where(x => x.Enterprise.Type == Model.EnterpriseType.宾馆)
+                .Where(x => x.DayNo == (( DateTime.Parse(txt_yijiedai3.Text) - item3.BeginDate).Days + 1));
+            if (temp_t_hotel.Count() > 0)
             {
-                rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3djs.Text }, StringSplitOptions.None).Count() > 1);
+                foreach (var item in temp_t_hotel)
+                {
+                    temp.t_hotel += item.Enterprise.Name + " ";
+                }
             }
             else
             {
-                rptGov3.DataSource = sm3;
+                temp.t_hotel = "无";
             }
+
+            var temp_t_scenic = item3.Routes
+                .Where(x => x.Enterprise.Type == Model.EnterpriseType.景点)
+                .Where(x => x.DayNo == ((DateTime.Parse(txt_yijiedai3.Text) - item3.BeginDate).Days + 1));
+            if (temp_t_scenic.Count() > 0)
+            {
+                foreach (var item in temp_t_hotel)
+                {
+                    temp.t_scenic += item.Enterprise.Name + " ";
+                }
+            }
+            else
+            {
+                temp.t_scenic = "无";
+            }
+            sm3.Add(temp);
         }
+        ////V.2012.10.27
+        ////团队名字非空
+        //if (!string.IsNullOrEmpty(txt_name3.Text))
+        //{
+        //    //地接社名字非空
+        //    if (!string.IsNullOrEmpty(txt_name3djs.Text))
+        //    {
+        //        sm3 = sm3.Where(x => x.Name.Split(new string[] { txt_name3djs.Text }, StringSplitOptions.None).Count() > 1).ToList();
+        //        rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3.Text }, StringSplitOptions.None).Count() > 1);
+        //    }
+        //    else
+        //    {
+        //        rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3.Text }, StringSplitOptions.None).Count() > 1);
+        //    }
+        //}
+        ////团队名字空
+        //else
+        //{
+        //if (!string.IsNullOrEmpty(txt_name3djs.Text))
+        //{
+        //    rptGov3.DataSource = sm3.Where(x => x.Name.Split(new string[] { txt_name3djs.Text }, StringSplitOptions.None).Count() > 1);
+        //}
+        //else
+        //{
+        //V.20120.10.30
+        rptGov3.DataSource = sm3;
+        //}
+        //}
         rptGov3.DataBind();
     }
 
@@ -281,6 +357,8 @@ class statics_Gov3
     public string Name { get; set; }//地接社名称
     public string GId { get; set; }
     public string Gname { get; set; }//团队名称
-    public string Playinfo { get; set; }
     public string Bedate { get; set; }//起止时间
+    public string y_hotel { get; set; }//昨天住宿
+    public string t_hotel { get; set; }//今天住宿
+    public string t_scenic { get; set; }//今天景区
 }
