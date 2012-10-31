966d46229e879bc7b4c46360f477ead329668065
phiree <phiree@gmail.com>
2012-10-31 15:36:52 +0800

    旅游门添加企业

--- MODEL/Area.cs
+++ MODEL/Area.cs
@@ -13,5 +13,38 @@ namespace Model
         public virtual string Code { get; set; }
         public virtual int AreaOrder { get; set; }
         public virtual string MetaDescription { get; set; }
+
+        /// <summary>
+        ///行政级别:省,市,区
+        /// </summary>
+        public AreaLevel Level
+        {
+            get
+            {
+                if (level == null)
+                {
+                    if (Code.EndsWith("0000"))
+                    {
+                        level = AreaLevel.省;
+                    }
+                    else if (Code.EndsWith("00"))
+                    {
+                        level = AreaLevel.市;
+                    }
+                    else
+                    {
+                        level = AreaLevel.区县;
+                    }
+                }
+                return level;
+            }
+        }
+        private AreaLevel level;
+    }
+    public enum AreaLevel
+    {
+        省,
+        市,
+        区县
     }
 }
