5a742f4b0b076f3c2ca8b9f7abf04acb671a5bee
phiree <phiree@gmail.com>
2012-11-20 17:40:26 +0800

    解决github不能上传的问题..可能会将代码覆盖..

--- WEB/ScenicManager/BillUnpaid.aspx.cs
+++ WEB/ScenicManager/BillUnpaid.aspx.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,8 +21,8 @@ public partial class ScenicManager_BillUnpaid : bpScenicManager
     private void bind()
     {
         bool? billState=null;
-        if (ddlCondition.Text == "已结单") billState = true;
-        if (ddlCondition.Text == "未结单") billState = false;
+        if (ddlCondition.Text =="已结单") billState = true;
+        if (ddlCondition.Text =="未结单") billState = false;
         CurrentScenic = Master.Scenic;
         rptStatis.DataSource = bllOrder.GetListForUser(0, CurrentScenic.Id, billState,txtbegin.Text.Trim(),txtend.Text.Trim());
         rptStatis.DataBind();
@@ -56,63 +55,5 @@ public partial class ScenicManager_BillUnpaid : bpScenicManager
     {
         ddlCondition_SelectedIndexChanged(null, null);
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using BLL;
 
-public partial class ScenicManager_BillUnpaid : bpScenicManager
-{
-    BLLOrder bllOrder = new BLLOrder();
-
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        if (!IsPostBack)
-        {
-            ddlCondition_SelectedIndexChanged(null, null);
-        }
-    }
-
-    private void bind()
-    {
-        bool? billState=null;
-        if (ddlCondition.Text == "已结单") billState = true;
-        if (ddlCondition.Text == "未结单") billState = false;
-        CurrentScenic = Master.Scenic;
-        rptStatis.DataSource = bllOrder.GetListForUser(0, CurrentScenic.Id, billState,txtbegin.Text.Trim(),txtend.Text.Trim());
-        rptStatis.DataBind();
-    }
-
-    protected void ddlCondition_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        if (txtbegin.Text == "" || txtend.Text == "")
-        {
-            txtbegin.Text = DateTime.Now.Year + "年" + "01月";
-            txtend.Text = DateTime.Now.Year + "年" + "12月";
-        }
-        DateTime begintime, endtime;
-        if (DateTime.TryParse(txtbegin.Text, out begintime) && DateTime.TryParse(txtend.Text, out endtime))
-        {
-            if (begintime > endtime)
-            {
-                ScriptManager.RegisterStartupScript(this, this.GetType(), "s", "alert('起始时间不得大于终止时间')", true);
-                return;
-            }
-        }
-        else
-        {
-            txtbegin.Text = DateTime.Now.Year + "年" + "01月";
-            txtend.Text = DateTime.Now.Year + "年" + "12月";
-        }
-        bind();
-    }
-    protected void btnSearch_Click(object sender, EventArgs e)
-    {
-        ddlCondition_SelectedIndexChanged(null, null);
-    }
->>>>>>> 3c47c6f56428a27d52e9e0d410aede14e6057a12
 }
