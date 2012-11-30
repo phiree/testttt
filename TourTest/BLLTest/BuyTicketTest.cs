c4186d85a5fbc86e6235c618912465e1c9f8e8fb
sst330381 <sst330381@gmail.com>
2012-11-30 18:46:48 +0800

    blltest

--- TourTest/BLLTest/BuyTicketTest.cs
+++ TourTest/BLLTest/BuyTicketTest.cs
@@ -6,14 +6,14 @@ using NUnit.Framework;
 
 namespace TourTest.BLLTest
 {
-[TestFixture]
-public class BuyTicketTest
-{
-[Test]
-[Ignore]
-public void nomethod()
-{ 
-        
-}
-}
+    [TestFixture]
+    public class BuyTicketTest
+    {
+        [Test]
+        [Ignore]
+        public void nomethod()
+        {
+
+        }
+    }
 }
--- TourTest/BLLTest/CommonUserTest.cs
+++ TourTest/BLLTest/CommonUserTest.cs
@@ -7,78 +7,78 @@ using Model;
 
 namespace TourTest.BLLTest
 {
-[TestFixture]
-public class CommonUserTest
-{
-    BLL.BLLCommonUser bllcommonuser=new BLL.BLLCommonUser();
-
-    [Test]
-    public void GetCommonUserByUserIdTest_rightcondition()
+    [TestFixture]
+    public class CommonUserTest
     {
-        var result = bllcommonuser.GetCommonUserByUserId(Guid.NewGuid());
-        Assert.IsTrue(result.Count == 0);
-    }
+        BLL.BLLCommonUser bllcommonuser = new BLL.BLLCommonUser();
 
-    [Test]
-    public void GetCommonUserByIdCardTest_null()
-    {
-        var result = bllcommonuser.GetCommonUserByIdCard(null);
-        Assert.IsTrue(result.Count == 0);
-    }
+        [Test]
+        public void GetCommonUserByUserIdTest_rightcondition()
+        {
+            var result = bllcommonuser.GetCommonUserByUserId(Guid.NewGuid());
+            Assert.IsTrue(result.Count == 0);
+        }
 
-    [Test]
-    public void GetCommonUserByIdCardTest_wrongid()
-    {
-        var result = bllcommonuser.GetCommonUserByIdCard("");
-        Assert.IsTrue(result.Count == 0);
-    }
+        [Test]
+        public void GetCommonUserByIdCardTest_null()
+        {
+            var result = bllcommonuser.GetCommonUserByIdCard(null);
+            Assert.IsTrue(result.Count == 0);
+        }
 
-    [Test]
-    public void GetCommonUserByUserIdandidcardTest_condition1()
-    {
-        var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(),null);
-        Assert.Null(result);
-    }
+        [Test]
+        public void GetCommonUserByIdCardTest_wrongid()
+        {
+            var result = bllcommonuser.GetCommonUserByIdCard("");
+            Assert.IsTrue(result.Count == 0);
+        }
 
-    [Test]
-    public void GetCommonUserByUserIdandidcardTest_condition2()
-    {
-        var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(), "");
-        Assert.Null(result);
-    }
+        [Test]
+        public void GetCommonUserByUserIdandidcardTest_condition1()
+        {
+            var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(), null);
+            Assert.Null(result);
+        }
 
-    [Test]
-    public void GetCommonUserByUserIdandidcardTest_condition3()
-    {
-        var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(), "123");
-        Assert.Null(result);     
-    }
+        [Test]
+        public void GetCommonUserByUserIdandidcardTest_condition2()
+        {
+            var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(), "");
+            Assert.Null(result);
+        }
 
-    [Test]
-    public void GetTest_null()
-    {
-        var result=bllcommonuser.Get(Guid.NewGuid(), null, null);
-        Assert.Null(result);
-    }
+        [Test]
+        public void GetCommonUserByUserIdandidcardTest_condition3()
+        {
+            var result = bllcommonuser.GetCommonUserByUserIdandidcard(Guid.NewGuid(), "123");
+            Assert.Null(result);
+        }
 
-    [Test]
-    public void SaveCommonUserTest_condition1()
-    { 
-        CommonUser cu = new CommonUser();
-        cu.Name = "小A";
-        cu.IdCard = "330381198812164236";
-        cu.User = new BLL.BLLMembership().GetMember("admin");
-        string message;
-        bllcommonuser.SaveCommonUser(cu,out message);
-        Assert.AreEqual(string.Empty, message);
+        [Test]
+        public void GetTest_null()
+        {
+            var result = bllcommonuser.Get(Guid.NewGuid(), null, null);
+            Assert.Null(result);
+        }
 
-        CommonUser cu2 = new CommonUser();
-        cu.Name = "小A";
-        cu.IdCard = "330381198812164236";
-        cu.User = new BLL.BLLMembership().GetMember("admin");
-        string message2;
-        bllcommonuser.SaveCommonUser(cu2, out message2);
-        Assert.AreEqual("已存在", message2);
+        [Test]
+        public void SaveCommonUserTest_condition1()
+        {
+            CommonUser cu = new CommonUser();
+            cu.Name = "小A";
+            cu.IdCard = "330381198812164236";
+            cu.User = new BLL.BLLMembership().GetMember("admin");
+            string message;
+            bllcommonuser.SaveCommonUser(cu, out message);
+            Assert.AreEqual(string.Empty, message);
+
+            CommonUser cu2 = new CommonUser();
+            cu.Name = "小A";
+            cu.IdCard = "330381198812164236";
+            cu.User = new BLL.BLLMembership().GetMember("admin");
+            string message2;
+            bllcommonuser.SaveCommonUser(cu2, out message2);
+            Assert.AreEqual("已存在", message2);
+        }
     }
-}
 }
