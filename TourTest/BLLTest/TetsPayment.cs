5c0ac88a88c472784d1147e01963a1158de9699c
phiree <phiree@gmail.com>
2012-12-07 10:21:57 +0800

    testfolder rename. delete  useless files

---
+++ TourTest/BLLTest/TetsPayment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace TourTest.TDD.Payment
+{
+    /// <summary>
+    /// 如何测试支付
+    /// 1 断言是什么: 
+    /// 
+    /// </summary>
+    [TestFixture]
+   public class TetsPayment
+    {
+
+        [Test]
+       public void TestPay()
+       {
+           string orderId = Guid.NewGuid().ToString();
+           TestCodePayment payment = new TestCodePayment();
+       }
+        [Test]
+        public void Pay()
+        {
+            Model.Order order = new BLL.BLLOrder().GetOrderByOrderid(62);
+            BLL.BLLPayment bllpamment = new BLL.BLLPayment(order);
+            bllpamment.Pay();
+            bllpamment.Received("dasf");
+            
+        }
+    }
+
+   public class TestCodePayment
+   {
+      public TestCodeOrder Order;
+      public void Pay(TestCodeOrder order)
+      { 
+        
+      }
+   }
+   public class TestCodeOrder
+   {
+       public bool HasPaid;
+    }
+
+    
+
+
+}
