80a115f4cbc1f2ba5750aa723bdaf6858b0e1c85
sst330381 <sst330381@gmail.com>
2013-01-29 11:43:16 +0800

    report new file

---
+++ AdminWeb/Manager/QuZhouSpring/TicketStatistics4.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Manager_QuZhouSpring_TicketStatistics4 : System.Web.UI.Page
+{
+    BLL.BLLOrder bllorder = new BLL.BLLOrder();
+    string s_scenicname = string.Empty;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        s_scenicname = Request["scenicname"] == null ? string.Empty : Request["scenicname"].ToString();
+        if (!IsPostBack)
+        {
+            BindData();
+        }
+    }
+    protected void BindData()
+    {
+        if (!string.IsNullOrWhiteSpace(s_scenicname))
+        {
+            rptStatic.DataSource = bllorder.GetDaysScenicOrderTotal(s_scenicname);
+            rptStatic.DataBind();
+        }
+    }
+}
