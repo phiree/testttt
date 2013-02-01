abdc974a7b6232358eaef045db1cc2e37f16199b
foreverjjj <502224593@qq.com>
2013-02-01 12:27:45 +0800

    123

--- WEB/ScenicManager/CheckTicket.aspx.cs
+++ WEB/ScenicManager/CheckTicket.aspx.cs
@@ -14,6 +14,7 @@ using System.Text.RegularExpressions;
 
 public partial class ScenicManager_CheckTicket : bpScenicManager
 {
+    123
     #region 公共的参数
     BLLTicketAssign bllticketassign = new BLLTicketAssign();
     BLLTicketPrice bllticketprice = new BLLTicketPrice();
