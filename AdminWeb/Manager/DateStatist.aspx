c0bfe24bbb2f85e5797f98a6b32c5d984b5e1378
foreverjjj <502224593@qq.com>
2013-02-02 13:50:18 +0800

    验票统计

---
+++ AdminWeb/Manager/DateStatist.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Manager/manager.master" AutoEventWireup="true" CodeFile="DateStatist.aspx.cs" Inherits="Manager_DateStatist" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphmain" Runat="Server">
+    <asp:Repeater ID="rptStatis" runat="server" 
+    onitemdatabound="rptStatis_ItemDataBound">
+        <HeaderTemplate>
+            <table border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td>
+                        时间
+                    </td>
+                    <td>
+                        景区
+                    </td>
+                    <td>
+                        验票人数
+                    </td>
+                </tr>
+            
+        </HeaderTemplate>
+        <ItemTemplate>
+            <tr>
+                <td>
+                    <%# Eval("date","{0:yyyy-MM-dd}")%>
+                </td>
+                <td>
+                    <%# Eval("ticket.Scenic.Name")%>
+                </td>
+                <td>
+                    <%# Eval("Amount") %>
+                </td>
+            </tr>
+        </ItemTemplate>
+    </asp:Repeater>
+</asp:Content>
+
---
+++ AdminWeb/Manager/DateStatist.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Model;
+using BLL;
+using System.Configuration;
+
+public partial class Manager_DateStatist : System.Web.UI.Page
+{
+    BLLTicket bllTicket = new BLLTicket();
+    BLLTicketAssign bllTa = new BLLTicketAssign();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            bindData();
+        }
+    }
+
+    private void bindData()
+    {
+        string quzhouDate = ConfigurationManager.AppSettings["quzhouDate"];
+        string ticket = ConfigurationManager.AppSettings["ticketId"];
+        List<Ticket> listTicket = new List<Ticket>();
+        foreach (string id in ticket.Split(','))
+        {
+            listTicket.Add(bllTicket.GetTicket(int.Parse(id)));
+        }
+        DateTime beginDate = DateTime.Parse(quzhouDate.Split(',')[0]);
+        DateTime endDate = DateTime.Parse(quzhouDate.Split(',')[1]);
+        List<table> listTable = new List<table>();
+        for (int i = 0; beginDate.AddDays(i)<=endDate; i++)
+        {
+            foreach (Ticket tt in listTicket)
+            {
+                table tab = new table();
+                tab.date = beginDate.AddDays(i);
+                tab.ticket = tt;
+                //为这次活动特殊判断，以后不能用
+                tab.Amount = bllTa.GetListByTimeAndScenic(beginDate.AddDays(i), beginDate.AddDays(i+1), tt.Scenic).Count;
+                listTable.Add(tab);
+            }
+        }
+        rptStatis.DataSource = listTable;
+        rptStatis.DataBind();
+
+    }
+    protected void rptStatis_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        if (e.Item.ItemType == ListItemType.AlternatingItem || e.Item.ItemType == ListItemType.Item)
+        {
+            table t = e.Item.DataItem as table;
+            if (t.Amount == 0)
+            {
+                e.Item.Visible = false;
+            }
+        }
+    }
+}
+
+public class table
+{
+    public DateTime date { get; set; }
+    public Ticket ticket { get; set; }
+    public int Amount { get; set; }
+}
