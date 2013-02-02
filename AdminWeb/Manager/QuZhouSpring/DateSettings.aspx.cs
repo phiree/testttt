5dcb23b1b162caedd3f503f7d74e4c0b7025f395
foreverjjj <502224593@qq.com>
2013-02-02 16:54:39 +0800

    bfb

--- AdminWeb/Manager/QuZhouSpring/DateSettings.aspx.cs
+++ AdminWeb/Manager/QuZhouSpring/DateSettings.aspx.cs
@@ -107,8 +107,9 @@ public partial class Manager_QuZhouSpring_DateSettings : System.Web.UI.Page
             {
                 Count += bllTa.GetListByTimeAndScenic(dt, dt.AddDays(1), ticket.Scenic).Count;
             }
-            laCheckedAmount.Text = Count.ToString();
+            laCheckedAmount.Text = Count.ToString() + "(" + (Count * 100.0 / solidamount).ToString("f2")+"%)";
             total3 += Count;
+
         }
         if (e.Item.ItemType == ListItemType.Footer)
         {
@@ -117,7 +118,7 @@ public partial class Manager_QuZhouSpring_DateSettings : System.Web.UI.Page
             Literal laTotal3 = e.Item.FindControl("laTotal3") as Literal;
             laTotal.Text = total1.ToString();
             laTotal2.Text = total2.ToString();
-            laTotal3.Text = total3.ToString();
+            laTotal3.Text = total3.ToString() + "(" + (total3 * 100.0 / total2).ToString("f2") + "%)";
         }
     }
 }
