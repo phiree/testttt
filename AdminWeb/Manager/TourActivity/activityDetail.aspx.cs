b8ba402925be314bbdc7332cac600f6b0fd15015
foreverjjj <502224593@qq.com>
2013-02-02 14:27:17 +0800

    das

--- AdminWeb/Manager/TourActivity/activityDetail.aspx.cs
+++ AdminWeb/Manager/TourActivity/activityDetail.aspx.cs
@@ -12,7 +12,10 @@ public partial class Manager_TourActivity_activityDetail : System.Web.UI.Page
     BLLTourActivity bllTourActivity = new BLLTourActivity();
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            bindDate();
+        }
     }
 
     private void bindDate()
@@ -20,7 +23,18 @@ public partial class Manager_TourActivity_activityDetail : System.Web.UI.Page
         if (Request.QueryString["actId"] != null)
         {
             string actId = Request.QueryString["actId"];
-            //bllTourActivity
+            TourActivity ta= bllTourActivity.GetOne(actId);
+            txtAmountPerIdcardInActivity.Text = ta.AmountPerIdcardInActivity.ToString();
+            txtAmountPerIdcardTicket.Text = ta.AmountPerIdcardTicket.ToString();
+            txtBeginDate.Text = ta.BeginDate.ToString("yyyy-MM-dd");
+            txtBeginHour.Text = ta.BeginHour.ToString();
+            txtBlack.Text = ta.AreasBlackList.ToString();
+            txtWhite.Text = ta.AreasWhiteList.ToString();
+            txtCode.Text = ta.ActivityCode.ToString();
+            txtEndDate.Text = ta.EndDate.ToString("yyyy-MM-dd");
+            txtEndHour.Text = ta.EndHour.ToString();
+            txtName.Text = ta.Name;
+            ckIsBuy.Checked = ta.AreasUseBlack;
         }
     }
 }
