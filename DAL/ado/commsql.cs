7189975e4ffb3459abc35fd0a884e7e2bf9747e8
phiree <phiree@gmail.com>
2013-02-05 16:17:39 +0800

    12301login调整, 数量规则判断使用原生sql

---
+++ DAL/ado/commsql.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+namespace DAL.ado
+{
+    //参与者获得门票的数量
+   public class commsql
+    {
+       SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["touronline"].ConnectionString);
+
+       public int IdCardAmountPerActivity(string activityCode,string idcard)
+       {
+           string sql = string.Format(@"select count(*)
+                                    from TicketAssign ta,OrderDetail detail
+                                    ,TicketPrice tp,Ticket t,TourActivity act
+                                    where ta.OrderDetail_id=detail.Id
+                                    and detail.TicketPrice_id=tp.Id
+                                    and tp.Ticket_id=t.Id
+                                    and t.TourActivity_id=act.Id
+                                    and ta.IdCard='{0}'
+                                    and ActivityCode='{1}'", idcard, activityCode);
+           return ExecuteScalarInt(sql);
+
+       }
+       public int IdCardAmountPerActivityPerTicket(string activityCode, string ticketcode, string idcard)
+       {
+           string sql = string.Format(@"select count(*)
+                                    from TicketAssign ta,OrderDetail detail
+                                    ,TicketPrice tp,Ticket t,TourActivity act
+                                    where ta.OrderDetail_id=detail.Id
+                                    and detail.TicketPrice_id=tp.Id
+                                    and tp.Ticket_id=t.Id
+                                    and t.TourActivity_id=act.Id
+                                    and ta.IdCard='{0}'
+                                    and ActivityCode='{1}'
+                                    and t.ProductCode='{2}'", idcard, activityCode, ticketcode);
+           return ExecuteScalarInt(sql);
+
+       }
+       public int ExecuteScalarInt(string sql)
+       {
+           SqlCommand comm = new SqlCommand(sql, conn);
+           OpenConn();
+           int scalarResult =(int) comm.ExecuteScalar();
+           conn.Close();
+           return scalarResult;
+       }
+
+       
+
+       public void OpenConn()
+       {
+            if (conn.State != System.Data.ConnectionState.Open)
+           {
+               conn.Open();
+           }
+       }
+       public void CloseConn()
+       {
+           if (conn.State != System.Data.ConnectionState.Closed)
+           {
+               conn.Close();
+           }
+       }
+    }
+}
